9a9a3b55e4dff353666574cebb902c7486e72839
xAnGGx <angenispb@mail.ru>
2022-04-07 17:32:47 +0300

    Added mask2base64 utils

---
+++ Utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import zlib
+from PIL import Image
+import base64
+import cv2
+import io
+
+def base64_2_mask(s):
+    z = zlib.decompress(base64.b64decode(s))
+    n = np.fromstring(z, np.uint8)
+    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
+    return mask
+
+def mask_2_base64(mask):
+    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
+    img_pil.putpalette([0,0,0,255,255,255])
+    bytes_io = io.BytesIO()
+    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
+    bytes = bytes_io.getvalue()
+    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
